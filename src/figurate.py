fb1d41e1debe4a61d305a1f11cd7a139cb124c11
Mack Ward <mackorone@gmail.com>
2017-02-10 10:28:46 -0500

    Remove INCREMENT from Figurate

--- src/figurate.py
+++ src/figurate.py
@@ -2,27 +2,21 @@ from sequence import Sequence
 
 
 class Figurate(Sequence):
-
-    _INCREMENT = None
-
     @classmethod
     def _append(cls):
-        cls._NUMS.append(2 * cls._NUMS[-1] - cls._NUMS[-2] + cls._INCREMENT)
+        cls._NUMS.append(
+            cls._NUMS[-1] * 2 - cls._NUMS[-2] +
+            cls._NUMS[ 1] - 1 - cls._NUMS[ 0]
+        )
 
 
 class Triangle(Figurate):
-
     _NUMS = [1, 3]
-    _INCREMENT = 1
 
 
 class Pentagonal(Figurate):
-
     _NUMS = [1, 5]
-    _INCREMENT = 3
 
 
 class Hexagonal(Figurate):
-
     _NUMS = [1, 6]
-    _INCREMENT = 4
