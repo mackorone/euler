871c91f393f005348d309b145a528164fc22ddec
Mack Ward <mackorone@gmail.com>
2017-02-13 02:21:10 -0500

    STILL working on 060.py

--- src/060.py
+++ src/060.py
@@ -1,40 +1,24 @@
-from collections import defaultdict
-from itertools import combinations
 from prime import Prime
-from tuples import gen_tuples
 
 
 def ans():
+    size = 5
+    groups = []
+    for p in Prime.gen_nums():
+        print(p)
+        for group in groups:
+            for e in group:
+                if not (
+                    Prime.contains(int(str(p) + str(e))) and
+                    Prime.contains(int(str(e) + str(p)))
+                ):
+                    break
+            else:
+                group.add(p)
+                if len(group) == size:
+                    return list(group)
+        groups.append({p})
 
-    disqualified = defaultdict(set)
-
-    for indices in gen_tuples(4):
-
-        primes = None
-        satisfies = True
-
-        for i, j in combinations(indices, 2):
-
-            if i in disqualified[j]:
-                satisfies = False
-                break
-
-            if not primes:
-                primes = {k: Prime.nth(k) for k in indices}
-                print(list(reversed(sorted(list(primes.values())))))
-
-            if not (
-                Prime.contains(int(str(primes[i]) + str(primes[j]))) and
-                Prime.contains(int(str(primes[j]) + str(primes[i])))
-            ):
-                disqualified[i].add(j)
-                disqualified[j].add(i)
-                satisfies = False
-                break
-
-        if satisfies:
-            return sum(primes.values())
-    
 
 if __name__ == '__main__':
     print(ans())
