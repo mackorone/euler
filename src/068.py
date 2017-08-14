c228a5bd2aac1affa896655b2000ddbce14c3ac3
Mack Ward <mackorone@gmail.com>
2017-08-14 18:38:03 -0400

    Solved 068

---
+++ src/068.py
@@ -0,0 +1,64 @@
+from itertools import permutations
+
+
+def get_groups(perm):
+    node_indices = [
+        (0, 5, 6),
+        (1, 6, 7),
+        (2, 7, 8),
+        (3, 8, 9),
+        (4, 9, 5),
+    ]
+    return [
+        [perm[j] for j in node_indices[i]]
+        for i in range(5)
+    ]
+
+
+def is_magic(perm):
+    prev_sum = None
+    for group in get_groups(perm):
+        sum_ = sum(group)
+        if not prev_sum:
+            prev_sum = sum_
+        if sum_ != prev_sum:
+            return False
+        prev_sum = sum_
+    return True
+
+
+def serialize(perm):
+    groups = get_groups(perm)
+    min_ = None
+    index = None
+    for i, group in enumerate(groups):
+        if not min_ or group[0] < min_:
+            min_ = group[0]
+            index = i
+    return int(''.join(
+        str(num)
+        for i in range(index, index + 5)
+        for num in groups[i % 5]
+    ))
+
+
+def ans():
+    # The insight is that a 16-digit string requires that the number 10 be an
+    # exterior node. However, in that case, the exterior nodes must be numbers
+    # 6-10 in order for the pentagon ring to be magic. Thus, rather than
+    # iterating over all permutations for numbers 1 through 10, we can iterate
+    # over 1 through 5 and 6 through 10 separately, which greatly reduces the
+    # amount of computation required.
+    magics = []
+    interior_perms = list(permutations(range(1, 6)))
+    exterior_perms = list(permutations(range(6, 11)))
+    for in_perm in interior_perms:
+        for ex_perm in exterior_perms:
+            perm = ex_perm + in_perm
+            if is_magic(perm):
+                magics.append(perm)  
+    return max(serialize(perm) for perm in magics)
+    
+
+if __name__ == '__main__':
+    print(ans())
